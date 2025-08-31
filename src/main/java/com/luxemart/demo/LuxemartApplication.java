250cc09d5f2091057cd0285eca4b7c28589ed583
Sayan Roy <85756676+Sayankroy@users.noreply.github.com>
2025-09-01 00:10:17 +0530

    Create project

---
+++ src/main/java/com/luxemart/demo/LuxemartApplication.java
@@ -0,0 +1,13 @@
+package com.luxemart.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxemartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxemartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luxemart/demo/LuxemartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxemart.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxemartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
